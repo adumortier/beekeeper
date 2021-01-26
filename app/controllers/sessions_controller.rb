ee77727dda738e7f592b4696eb2e7d2a6d820707
Alexis Dumortier <dumortier.alexis@gmail.com>
2021-01-26 23:52:49 +0100

    Fix table mobile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,9 +9,9 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email].downcase)
     if user && user.authenticate(params[:password])
       login(user)
-      flash[:success] = "Bienvenue #{user.first_name}!"
+      flash[:success] = "Bienvenue #{user.first_name} !"
     else
-      flash[:danger] = "Email ou mot de passe incorrect"
+      flash[:danger] = "Email ou mot de passe incorrect."
       redirect_to login_path and return
     end
     redirect_to (current_user.admin? ? root_path : reservation_path)
