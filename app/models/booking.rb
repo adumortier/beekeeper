8a0adc4814ea48e80606338c1003f75a900d75cb
Alexis Dumortier <dumortier.alexis@gmail.com>
2021-02-01 16:49:24 +0100

    Change booking validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,12 +4,12 @@ class Booking < ApplicationRecord
   has_many :booking_products, dependent: :destroy
   has_many :products, through: :booking_products
 
-  validates_presence_of :comment, optional: true
+  validates_presence_of :comment, allow_blank: true
 
   enum status: %w(pending notified)
 
   def has_comment?
-    return true unless comment.nil?
+    return true unless (comment.nil? || comment == '')
   end
   
 end
